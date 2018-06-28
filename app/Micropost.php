211c4bb9bb7dab98c045ff27bdf4c370b8b6b764
EC2 Default User <ec2-user@ip-172-31-7-4.ec2.internal>
2018-06-28 14:42:29 +0000

    post

---
+++ app/Micropost.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Micropost extends Model
+{
+    protected $fillable = ['content', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', 'MicropostsController@index');
 
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
@@ -25,4 +26,9 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
