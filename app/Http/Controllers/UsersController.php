b4908d1a57bc57fe816c5372c5bf56d6b78f0c64
EC2 Default User <ec2-user@ip-172-31-7-4.ec2.internal>
2018-07-11 11:47:11 +0000

    timeline

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,7 +20,7 @@ class UsersController extends Controller
     public function show($id)
     {
         $user = User::find($id);
-        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+        $microposts = $user->feed_microposts()->orderBy('created_at', 'desc')->paginate(10);
 
         $data = [
             'user' => $user,
